08968a73b05d680c603c990e485b7f317e2479d8
Roberth <robinmoralesquiroz@gmail.com>
2024-06-20 13:02:56 -0500

    update models and routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 
-Route::get('categories', [CategoryController::class, 'index']);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('categories/{id}', [CategoryController::class, 'show']);
 Route::get('categories/{id}/products', [CategoryController::class, 'getProductsInCategory']);
 Route::get('categories/{id}/product-count', [CategoryController::class, 'getProductCountInCategory']);
